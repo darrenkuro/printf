792946b44c31e9097df38872b730f3b38974507b
dlu <dlu@student.42berlin.de>
2023-05-03 11:43:21 +0200

    scaffolding

---
+++ srcs/ft_printf.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 10:58:18 by dlu               #+#    #+#             */
+/*   Updated: 2023/05/03 11:42:43 by dlu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+typedef struct 
+
+void	print_char(char **s, int *count)
+{
+	write(1, (*s)++, 1);
+	++count;
+}
+
+void	print_arg(char **s, va_list *args, int *count)
+{
+
+}
+
+int	parse(char *s, va_list *args)
+{
+	int		count;
+
+	count = 0;
+	while (*s)
+	{
+		if (*s != '%')
+			print_char(&s, &count);
+		else
+			print_arg(&s, args, &count);
+	}
+	return (count);
+}
+
+int	ft_printf(const char *s, ...)
+{
+	int		count;
+	va_list	args;
+
+	va_start(args, s);
+	count = parse((char *) s, &args);
+	va_end(args);
+	return (count);
+}
+
+int	main(void)
+{
+	ft_printf("abcdefg", "abcd");
+	return (0);
+}
