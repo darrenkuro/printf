9205199658b5116167b0947e57b15110799e819c
dlu <dlu@student.42berlin.de>
2023-05-04 17:27:31 +0200

    cleanup header

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dlu <dlu@student.42berlin.de>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 10:31:59 by dlu               #+#    #+#             */
-/*   Updated: 2023/05/04 17:23:14 by dlu              ###   ########.fr       */
+/*   Updated: 2023/05/04 17:24:48 by dlu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,13 @@
 
 # include <stdarg.h>
 # include <unistd.h>
-# include <stdio.h>
 
 # define DEC		"0123456789"
 # define HEXU		"0123456789ABCDEF"
 # define HEXL		"0123456789abcdef"
 # define NULL_STR	"(null)"
 # define NULL_PTR	"(nil)"
-# define NULL_LEN	6
 # define NBR_SIZE	23
-# define LL_MIN		-9223372036854775808
-# define LL_MINLEN	20
 # define INT_MIN	-2147483648
 # define INT_MINLEN	11
 
@@ -53,14 +49,13 @@ int		print_str(char *s, int *count, t_format format);
 void	print_char(char c, int *count, t_format format);
 void	print_ptr(void *p, int *count, t_format format);
 void	print_nbr(int n, char *base, int *count, t_format format);
-void	print_nbr_u(unsigned int n, char *base, int *count, t_format format);
 
 int		ft_strlen(char *s, t_format format);
 int		print_padding(char c, int len);
-void	load_nbr(int n, const char *base, t_format *format);
 int		len_nbr(int n, t_format *format);
-void	load_nbr_u(unsigned int n, const char *base, t_format *format);
 int		len_nbr_u(unsigned long long n, t_format *format);
+void	load_nbr(int n, const char *base, t_format *format);
+void	load_nbr_u(unsigned int n, const char *base, t_format *format);
 void	load_nbr_ptr(void *p, const char *base, t_format *format);
 
 #endif
