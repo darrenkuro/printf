0e5fbbc04be6768fab3acd9e02a7da73d2716a2d
dlu <dlu@student.42berlin.de>
2023-05-05 17:16:11 +0200

    includes libft and gitignore

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlu <dlu@42berlin.de>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/21 07:38:27 by dlu               #+#    #+#             */
+/*   Updated: 2023/04/26 06:01:28 by dlu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*ret;
+	t_list	*tmp;
+
+	if (!lst || !f)
+		return (NULL);
+	ret = NULL;
+	while (lst)
+	{
+		tmp = ft_lstnew((*f)(lst -> content));
+		if (!tmp)
+		{
+			ft_lstclear(&ret, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&ret, tmp);
+		lst = lst -> next;
+	}
+	return (ret);
+}
